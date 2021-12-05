4cacc1d6a2edd3de6cc8766272e7fbfac250a632
Trịnh Văn Tú <91907757+trinhvantu01@users.noreply.github.com>
2021-12-05 23:17:06 +0700

    fix css font chữ cart
    lúc chạy bài nhớ ấn Ctrl + shift+ r để  loat lại css

--- client/views/homepage/lienhe.php
+++ client/views/homepage/lienhe.php
@@ -57,30 +57,30 @@
             <form action="" method="post">
                 <div class="md:flex items-center mt-12">
                     <div class="w-full md:w-1/2 flex flex-col">
-                        <label class="font-semibold leading-none text-gray-300">Name</label>
+                        <label class="font-semibold leading-none text-gray-300">Họ Tên</label>
                         <input type="text" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
                     </div>
                     <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label class="font-semibold leading-none text-gray-300">Phone</label>
+                        <label class="font-semibold leading-none text-gray-300">Điện Thoại</label>
                         <input type="email" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
                     </div>
                 </div>
                 <div class="md:flex items-center mt-8">
                     <div class="w-full flex flex-col">
-                        <label class="font-semibold leading-none text-gray-300">Subject</label>
+                        <label class="font-semibold leading-none text-gray-300">email</label>
                         <input type="text" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
                     </div>
 
                 </div>
                 <div>
                     <div class="w-full flex flex-col mt-8">
-                        <label class="font-semibold leading-none text-gray-300">Message</label>
+                        <label class="font-semibold leading-none text-gray-300">Nội dung</label>
                         <textarea type="text" class="h-40 text-base leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
                     </div>
                 </div>
                 <div class="flex items-center justify-center w-full">
                     <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
-                        Send message
+                        Gửi đi
                     </button>
                 </div>
             </form>
