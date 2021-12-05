546457da7094524de6bd8d3fae727a080d424c86
Trịnh Văn Tú <91907757+trinhvantu01@users.noreply.github.com>
2021-12-05 16:42:53 +0700

    add code tin tức

--- client/views/homepage/tintuc.php
+++ client/views/homepage/tintuc.php
@@ -1,16 +1,15 @@
 <!-- Create By Joker Banny -->
-<div class="min-h-screen #FFFFFF flex justify-center items-center py-20">
+<div class="min-h-screen  items-center ">
     <div class="container mx-auto p-12 bg-gray-100 rounded-xl">
-
         <h1 class="text-4xl uppercase font-bold from-current mb-8">Những Tin Tức Đáng Chú Ý</h1>
         <!-- Box-1 -->
-        <?php foreach ($tintuc as $item) : ?>
-            <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
+        <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0 ">
+            <?php foreach ($tintuc as $item) : ?>
                 <div class="bg-white">
                     <div>
                         <div class="shadow-lg hover:shadow-xl transform transition duration-500 hover:scale-105">
                             <div>
-                                <img class="w-full" src="<?= $item['anh'] ?>" />
+                                <img class="w-full" src="<?= PUBLIC_PATH . 'img/' . $item['anh'] ?>" />
                                 <div class="px-4 py-2">
                                     <h1 class="text-xl font-gray-700 font-bold"><?= $item['tieu_de'] ?></h1>
                                     <div class="flex space-x-2 mt-2">
@@ -19,16 +18,14 @@
                                     </div>
                                     <p class="text-sm tracking-normal"><?= $item['nd_ngan'] ?></p>
                                     <button class="mt-12 w-full text-center bg-yellow-400 py-2 rounded-lg">Read more</button>
+
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- Box-2 -->
+            <?php endforeach; ?>
+        </div>
 
-                <!-- Box-3 -->
-
-            </div>
-        <?php endforeach; ?>
     </div>
 </div>
