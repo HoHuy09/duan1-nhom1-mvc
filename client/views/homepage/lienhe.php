edc0cf6434bce3652823ca050dd774c2fa8a4c67
Trịnh Văn Tú <91907757+trinhvantu01@users.noreply.github.com>
2021-12-06 21:35:15 +0700

    add fix css liên hệ

--- client/views/homepage/lienhe.php
+++ client/views/homepage/lienhe.php
@@ -6,13 +6,13 @@
             </h1>
         </div>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
+            <div class="w-full bg-gray-900 rounded-lg   sahdow-lg p-12 flex flex-col justify-center items-center">
                 <div class="mb-8">
                     <img class="object-center object-cover rounded-full h-36 w-36" src="https://vieclam247pro.vn/uploads/tiny_uploads/Giang/it%20m%E1%BB%81m.jpg" alt="photo">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">Hồ Thích Huy</p>
-                    <p class="text-base text-gray-400 font-normal">Kỹ Sư Phần Mềm</p>
+                    <p class="text-2xl text-white font-bold mb-2">Hồ Thích Huy</p>
+                    <p class="text-base text-xl text-gray-400 font-normal">Kỹ Sư Phần Mềm</p>
                 </div>
             </div>
             <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
@@ -20,8 +20,8 @@
                     <img class="object-center object-cover rounded-full h-36 w-36" src="https://cdn.nguyenkimmall.com/images/detailed/555/may-anh-cho-nguoi-moi.jpg" alt="photo">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">Lê Ngọc Quỳnh</p>
-                    <p class="text-base text-gray-400 font-normal">Người Thiết Kế Đồ Họa</p>
+                    <p class="text-2xl text-white font-bold mb-2">Lê Ngọc Quỳnh</p>
+                    <p class="text-base text-xl text-gray-400 font-normal">Người Thiết Kế Đồ Họa</p>
                 </div>
             </div>
             <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
@@ -29,8 +29,8 @@
                     <img class="object-center object-cover rounded-full h-36 w-36" src="https://lasun.vn/wp-content/uploads/2018/11/con-nguoi-co-vai-tro-het-suc-quan-trong-600x300.jpg" alt="photo">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">Trịnh Văn Tú</p>
-                    <p class="text-base text-gray-400 font-normal">Hoạt Động Phát Triển</p>
+                    <p class="text-2xl text-white font-bold mb-2">Trịnh Văn Tú</p>
+                    <p class="text-base text-xl text-gray-400 font-normal">Hoạt Động Phát Triển</p>
                 </div>
             </div>
             <div class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center">
@@ -38,8 +38,8 @@
                     <img class="object-center object-cover rounded-full h-36 w-36" src="https://h2tsolution.vn/wp-content/uploads/image0034-1200x800.jpg" alt="photo">
                 </div>
                 <div class="text-center">
-                    <p class="text-xl text-white font-bold mb-2">Nguyễn Thành Đạt</p>
-                    <p class="text-base text-gray-400 font-normal">Chuyên Viên Marketings</p>
+                    <p class="text-2xl text-white font-bold mb-2">Nguyễn Thành Đạt</p>
+                    <p class="text-base text-xl  text-gray-400 font-normal">Chuyên Viên Marketings</p>
                 </div>
             </div>
 
@@ -57,29 +57,29 @@
             <form action="" method="post">
                 <div class="md:flex items-center mt-12">
                     <div class="w-full md:w-1/2 flex flex-col">
-                        <label class="font-semibold leading-none text-gray-300">Họ Tên</label>
+                        <label class="font-semibold text-2xl leading-none text-gray-300">Họ Tên</label>
                         <input type="text" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
                     </div>
                     <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label class="font-semibold leading-none text-gray-300">Điện Thoại</label>
+                        <label class="font-semibold text-2xl eading-none text-gray-300">Điện Thoại</label>
                         <input type="email" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
                     </div>
                 </div>
                 <div class="md:flex items-center mt-8">
                     <div class="w-full flex flex-col">
-                        <label class="font-semibold leading-none text-gray-300">email</label>
+                        <label class="font-semibold text-2xl leading-none text-gray-300">email</label>
                         <input type="text" class="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
                     </div>
 
                 </div>
                 <div>
                     <div class="w-full flex flex-col mt-8">
-                        <label class="font-semibold leading-none text-gray-300">Nội dung</label>
+                        <label class="font-semibold text-2xl leading-none text-gray-300">Nội dung</label>
                         <textarea type="text" class="h-40 text-base leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
                     </div>
                 </div>
                 <div class="flex items-center justify-center w-full">
-                    <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                    <button class="mt-9 text-2xl font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
                         Gửi đi
                     </button>
                 </div>
